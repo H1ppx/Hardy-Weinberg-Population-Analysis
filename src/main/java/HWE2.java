0ab8e2c8135a916c9b3e316a6c0e27b254584a06
Will Chu <willcty@gmail.com>
2018-12-14 12:04:56 -0500

    jfx running, google sheets oauth needs work again

--- src/main/java/HWE2.java
+++ src/main/java/HWE2.java
@@ -1,3 +1,18 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.BatchUpdateValuesRequest;
+import com.google.api.services.sheets.v4.model.BatchUpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -5,20 +20,23 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
+import java.io.*;
 import java.lang.reflect.Array;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class HWE2 extends Application {
 
-    double startP;
-    int genAmount;
-    static int amountOfPopulations;
-    ArrayList<Integer>populationSizes = new ArrayList<>();
+    private static double startP;
+    private static int genAmount;
+    private static int amountOfPopulations;
+    private static ArrayList<Integer>populationSizes = new ArrayList<>();
 
-    static ArrayList<XYChart.Series>series = new ArrayList<>();
-
+    private static ArrayList<XYChart.Series>series = new ArrayList<>();
 
     public HWE2(){
         Scanner keyboard = new Scanner(System.in);
@@ -53,8 +71,7 @@ public class HWE2 extends Application {
         for (int i = 0; i < amountOfPopulations; i++){
             series.get(i).setName("Population:" + populationSizes.get(i));
 
-            ArrayList<Double> pVals = getP(startP, genAmount, populationSizes.get(i));
-            System.out.println(pVals);
+            ArrayList<Double> pVals = getP(startP, populationSizes.get(i), genAmount);
             for (int j = 0; j<=pVals.size()-1; j++){
                 double currentP = pVals.get(j);
                 series.get(i).getData().add(new XYChart.Data(j, currentP));
@@ -73,28 +90,81 @@ public class HWE2 extends Application {
         stage.show();
     }
 
-    private void runGoogleSheets(){
+    public static void runGoogleSheets() throws IOException, GeneralSecurityException {
+        final String APPLICATION_NAME =
+                "Google Sheets API Java Quickstart";
 
+        final java.io.File DATA_STORE_DIR = new java.io.File(
+                System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart");
+
+        FileDataStoreFactory DATA_STORE_FACTORY;
+        final JsonFactory JSON_FACTORY =
+                JacksonFactory.getDefaultInstance();
+
+        HttpTransport HTTP_TRANSPORT;
+
+        final List<String> SCOPES =
+                Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+
+        HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+
+        // Build a new authorized API client service.
+        InputStream in    = new FileInputStream(
+                System.getProperty("user.dir")+"\\client_secret.json");
+
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // https://docs.google.com/spreadsheets/d/1AljqFAAp91zZOyBGmx3mjMWjmvXJjRRBAWFvhV20lNw/edit
+        String spreadsheetId = "1hj8MVQLchcMSyVkMPl66osQ8D1tE6DGkSQ-JBJxDU_c";
+
+        String sheet ="Lab!";
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        ArrayList<String> ranges = new ArrayList<>();
+        ArrayList<List<List<Object>>> values = new ArrayList<>();
+        for (int i = 1; i <= populationSizes.size();i++) {
+            values.add(Arrays.asList(Arrays.asList(getP(startP, populationSizes.get(i-1), genAmount))));
+            ranges.add(sheet+alphabet.charAt(i)+"56:Z");
+        }
+
+        List<ValueRange> data = new ArrayList<ValueRange>();
+        for (int i = 1; i<= values.size(); i++){
+            data.add(new ValueRange()
+                    .setRange(ranges.get(i-1))
+                    .setValues(values.get(i-1)));
+        }
+
+
+        BatchUpdateValuesRequest body = new BatchUpdateValuesRequest()
+                .setValueInputOption("USER_ENTERED")
+                .setData(data);
+        BatchUpdateValuesResponse result =
+                service.spreadsheets().values().batchUpdate(spreadsheetId, body).execute();
+        System.out.printf("%d cells updated.", result.getTotalUpdatedCells());
     }
 
-
-    private static void runJFX(String[] args){
+    public static void runJFX(String[] args){
         if (series.size() == amountOfPopulations){
             launch(args);
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
     private static double getNextP(double curretP, int populationSize) {
         double a = 0, b = 0;
@@ -108,7 +178,7 @@ public class HWE2 extends Application {
         return a/(a+b);
     }
 
-    private ArrayList<Double> getP(final double startP, final int populationSize, final int generations) {
+    private static ArrayList<Double> getP(final double startP, final int populationSize, final int generations) {
         double p = startP;
         ArrayList<Double> pVals = new ArrayList<>();
         for (int i = 0; i <= generations; i++) {
@@ -119,7 +189,4 @@ public class HWE2 extends Application {
         return pVals;
     }
 
-    public static void main(String[] args) {
-        runJFX(args);
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,23 @@
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter 1 for google charts, Enter 2 for jfx chart");
-        final double response = Double.parseDouble(keyboard.next());
-        if(response == 1){
-            GSheets.manual();
-        } else {
-            jfxApp.main(args);
+        System.out.println("GSheets: Enter 1, JFX: Enter 2");
+        int dispOption = Integer.parseInt(keyboard.next());
+        switch(dispOption){
+            case 1:
+                HWE2.runGoogleSheets();
+                break;
+
+            case 2:
+                HWE2.runJFX(args);
+                break;
         }
+
     }
+
 }
